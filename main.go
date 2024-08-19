690b5975cda4836541debb1d765c0eee3d363154
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-08-19 21:26:49 +0600

    Setting up an event model and registering a post route.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"example.com/event-booking-rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,10 +11,25 @@ func main(){
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") //localhost:8080
 }
 
 func getEvents(context *gin.Context){
-	context.JSON(http.StatusOK, gin.H{"message" : "Hello!"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent (context *gin.Context){
+	var event models.Event
+	err := context.ShouldBindBodyWithJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+	event.ID = 1
+	event.UserId = 1
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
