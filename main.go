d5c32312a5892c83b7c8ad2406f88098013794ea
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-08-19 23:20:26 +0600

    Adding database connection.

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"example.com/event-booking-rest-api/db"
 	"example.com/event-booking-rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main(){
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
