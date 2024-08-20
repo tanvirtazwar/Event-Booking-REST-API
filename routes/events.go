695363c136463ede5a07526701086e85826eb45a
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-08-20 13:51:33 +0600

    Updating Events

--- routes/events.go
+++ routes/events.go
@@ -49,3 +49,32 @@ func createEvent(context *gin.Context) {
 	}
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
+
+func updateEvent(context *gin.Context){
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+	_, err = models.GetEvenByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+	var updateEvent models.Event
+	err = context.ShouldBindBodyWithJSON(&updateEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	updateEvent.ID = eventId
+	err = updateEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update events. Try again later."+ err.Error()})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Updated event succesfully"})
+}
